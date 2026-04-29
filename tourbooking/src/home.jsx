359ca8439f541c64f789d1afc40517e80e7cce0f
Payam Haider <payamhaider64@gmail.com>
2026-04-29 16:02:25 +0500

    first commit

--- tourbooking/src/home.jsx
+++ tourbooking/src/home.jsx
@@ -1,11 +1,7 @@
+
 function home(){
     <>
-    <nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
     <h1>Payam haider</h1>
-  </div>
-</nav>
-
     </>
 }
 export default home;
